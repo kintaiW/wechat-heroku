41ad58c9a0bbed4d21e3887d4b11de7c278226c2
kintaiW <cscroll@foxmail.com>
2017-07-04 22:22:44 +0800

    change request

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -10,17 +10,19 @@ class WechatController < ApplicationController
 
   #接收微信服务器信息  
   def process_request
-    if check_signature?(params[:signature], params[:timestamp], params[:nonce]) #验证消息真实性  
+    # if check_signature?(params[:signature], params[:timestamp], params[:nonce]) #验证消息真实性  
       if params[:xml][:MsgType] == "event"
         if params[:xml][:Event] == "subscribe"
           render "wechat/info", layout: false, :formats => :xml          #关注  
         end
       else
-        render "wechat/info", layout: false, :formats => :xml  #用户输入消息时，回送欢迎关注  
+        if params[:xml][:MsgType] == "text"
+          render "wechat/info", :formats => :xml  #用户输入消息时，回送欢迎关注  
+        end
       end
-    end
+    # end
   end
-  
+
   private
   def check_signature?(signature,timestamp,nonce)
     Digest::SHA1.hexdigest([timestamp,nonce,@@token].sort.join) == signature
